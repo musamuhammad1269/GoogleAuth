3ddf473b6e5c686b7316217dda0331efb6aeee53
Musa <musamoh2002@gmail.com>
2024-05-29 09:14:31 -0700

    first commit

---
+++ src/main/java/com/authenticator/app/authenticatorApp/AuthenticatorAppApplication.java
@@ -0,0 +1,13 @@
+package com.authenticator.app.authenticatorApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticatorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticatorAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/authenticator/app/authenticatorApp/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.authenticator.app.authenticatorApp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests -> authorizeRequests
+                        .antMatchers("/", "/login**", "/error**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/", true)
+                );
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
